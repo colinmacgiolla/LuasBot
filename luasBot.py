1a4de7c15d3b3ff0e57c6fe289b9ca6de390d498
Colin MacGiollaEain <colin@arista.com>
2023-05-16 10:40:16 +0100

    Code update - added the following: * logging * debug and post modes * check if there has actually been an update before posting

--- luasBot.py
+++ luasBot.py
@@ -5,60 +5,96 @@ import requests, unicodedata
 import os
 from bs4 import BeautifulSoup
 from mastodon import Mastodon
+import logging
 
-
-DEBUG = True
-
+DEBUG = False
+POST = True
 
 def main():
-    
-    global DEBUG
-    
+
+    global DEBUG, POST
+
+    if DEBUG:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(name)s - %(message)s'
+        )
+    logging.basicConfig(filename='app.log', filemode='w', level=logging.DEBUG)
+
+
+    logging.info("Scraping luas.ie website")
     URL = "https://luas.ie/travel-updates/"
-    page = requests.get(URL)
-    
+    try:
+        page = requests.get(URL)
+    except Exception as e:
+        logging.error(e)
+        return -1
+
     soup = BeautifulSoup(page.content, "html.parser")
     results = soup.find_all("div", class_="page-content")
-  
+
     data = results[0].text.split("\n")
-    
+
+    logging.debug("Extracted the following data:\n%s" % data)
     # timestamp = next(datefinder.find_dates(results[0].text))
     timestamp = data[5]
-    
+    logging.info("Timestamp from post is: %s" % timestamp)
+
     luas_update = []
     for line in data:
         if "Red" in line or "Green" in line:
             luas_update.append( unicodedata.normalize("NFKD", line) )
 
-    toot = ["Luas update at: %s.\n\n" % timestamp]
+    toot = ["Luas update at: %s.\n" % timestamp, "\n"]
     #print(f"Luas update at {timestamp}\n")
 
     for line in luas_update:
         for sentence in line.split("."):
-            if "Green" in sentence or "lift" in sentence:
-                #print("\n")
-                toot.append("\n")
             if "Kind Regards" in sentence:
                 break
-            #print(f"{sentence.strip()}. ", end="")
-            toot.append(f"{sentence.strip()}. ")
-    
+
+            if "Green" in sentence or "lift" in sentence:
+                temp = toot[-1]
+                toot[-1] = temp+"\n"
+
+            toot.append(f"{sentence.strip()}.")
+
     if not DEBUG:
         # Avoid spamming people subscribing to tags while I'm testing
-        toot.append("\n\n")
+        toot.append("\n")
+        toot.append("\n")
         toot.append("#Luas #Dublin #MastaDaoine")
-            
-    #print("\n")
-    
-    mastodon = Mastodon(
-        access_token=os.environ.get('secret.mastodon_token'),
-        api_base_url="https://botsin.space/"
-    )
-    mastodon.status_post(''.join(toot) )
-    
-    
-    return None
 
+    logging.debug("Toot prepared: \n%s" % toot)
+
+
+    if os.path.exists("toot.text"):
+        with open("toot.text", "r") as f:
+            old_toot = f.readlines()
+            if old_toot == toot:
+                logging.info("No update found")
+                return
+
+
+    with open("toot.text", "w") as f:
+        f.writelines(toot)
+
+    if POST:
+        logging.info("Posting to Mastodon")
+        mastodon = Mastodon(
+            access_token=os.environ.get('mastodon_token'),
+            api_base_url="https://botsin.space/"
+        )
+        try:
+            mastodon.status_post(''.join(toot) )
+        except Exception as e:
+            logging.error(e)
+            return -1
+
+
+    return None
 
 
 if __name__ == "__main__":
